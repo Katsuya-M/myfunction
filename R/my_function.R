810880f9a4bf5bad06c5134ac5e7d19791aa64f5
Katsuya Miyajima <katsu.zima@gmail.com>
2020-01-07 10:01:33 +0900

    add colour_platte

---
+++ R/my_function.R
@@ -0,0 +1,174 @@
+#'myfunction
+#'
+#'
+#'@export
+
+se <- function(x) {
+  if(length(x) < 3) {
+   0
+  } else {
+    sd(x) / sqrt(length(x))
+  }
+}
+
+mean_bar <- function(x, mult = 1) {
+  x <- stats::na.omit(x)
+  mean <- mean(x)
+  data.frame(y = mean, ymin = mean, ymax = mean)
+}
+
+y.scale <- function(x) {
+  x <- x[!is.na(x)]
+  mx <- max(x)
+  b <- mx * 7 / 6
+  if (b == 0) {
+    s <- 0
+    msd <- 0
+  } else {
+    s <- floor(log10(b))
+    msd <- b * 10 ^ ((-1) * s)
+  }
+
+  ym <- if(msd < 1.8) {
+    if((ceiling(msd * 10)) %% 2 == 1) {
+      ceiling(msd * 10) * 10 ^ (s - 1)-(1 * 10 ^ (s - 1))
+    }else ceiling(msd * 10) * 10 ^ (s - 1)
+  }else if(floor(msd) < 4) {
+    if((msd*10)-(floor(msd)*10) < 5){
+      (floor(msd)*10+5)*10^(s-1)
+    }else (floor(msd)*10+10)*10^(s-1)
+  }else{
+    ceiling(msd)*10^(s)
+  }
+
+  if(ym*10^((-1)*s) == 1.4 | ym*10^((-1)*s) == .2*7){
+    ym <- ym * 1.5/1.4
+  }
+  if(ym*10^((-1)*s) == 7){
+    ym <- ym * 8/7
+  }
+  if(ym*10^((-1)*s) == 9){
+    ym <- ym + 10^(s)
+  }
+
+  yby <- if(ym*10^(-s) < 1.5){
+    2*10^(s-1)
+  }else if (ym*10^(-s) < 1.6){
+    3*10^(s-1)
+  }else if(ym*10^(-s) <1.7 ){
+    4*10^(s-1)
+  }else if(ym*10^(-s) <1.9 ){
+    6*10^(s-1)
+  }else if(ym*10^(-s) <2.0 ){
+    2*10^(s-1)
+  }else if(ym*10^(-s) <4){
+    5*10^(s-1)
+  }else if(ym*10^(-s) <8){
+    1*10^(s)
+  }else if(ym*10^(-s) <9){
+    2*10^(s)
+  }else 20*10^(s-1)
+
+  f <- max(x, na.rm = T) + yby/4
+  g <- f + yby/4
+  return(list(ymax = ym, yscale = seq(0, ym, by = yby), yby = yby, f = f, g = g))
+}
+
+mean_se_katsu <- function(x, mult = 1){
+  x <- stats::na.omit(x)
+  se <- if (length(x) < 3){
+    NA
+  }else{
+    mult * sqrt(stats::var(x)/length(x))
+  }
+  mean <- mean(x)
+  data.frame(y = mean, ymin = mean - se, ymax = mean + se)
+}
+
+
+
+ss <- mark_pvalue <-  function(x) {
+  sapply(x, function(x) {
+  if (is.na(x)) {
+    ""
+  } else if (x == "NaN") {
+    ""
+  } else if (x < 0.001) {
+    "***"
+  } else if (x < 0.01) {
+    "**"
+  } else if (x < 0.05) {
+    "*"
+  } else {
+    ""
+  }})
+}
+
+
+chart.csv <- function(path, height.base = F) {
+  require("tidyverse")
+  require("magrittr")
+  x <- list.files(path) %>% str_subset("^Chart.*\\.csv$")
+
+  if (height.base == F) {
+    d <- read_csv(paste0(path, "/", x[[1]])) %>%
+      dplyr::select(-`height-base[uV]`)
+    for (i in 2:length(x)) {
+      d1 <- read_csv(paste0(path, "/", x[[i]])) %>%
+        dplyr::select(-`height-base[uV]`)
+      names(d1)[2] <- paste("height", i)
+      d <- left_join(d, d1, by = "time[min]")
+    }
+    d <- d %>% set_colnames(c("time", paste(x, "(nA)"))) %>%
+      mutate_at(-1, ~ ./10000)
+  } else {
+    d <- read_csv(paste0(path, "/", x[[1]])) %>%
+      dplyr::select(-`CH1 height[uV]`)
+    for (i in 2:length(x)) {
+      d1 <- read_csv(paste0(path, "/", x[[i]])) %>%
+        dplyr::select(-`CH1 height[uV]`)
+      names(d1)[2] <- paste("height", i)
+      d <- left_join(d, d1, by = "time[min]")
+    }
+    d <- d %>% set_colnames(c("time", paste(x, "(nA)"))) %>%
+      mutate_at(-1, ~ ./10000)
+     }
+
+
+  return(d)
+}
+
+theme_my <- function (base_size = 25, base_family = "", base_line_size = .8,
+                      base_rect_size = .8) {
+  theme_bw(base_size = base_size, base_family = base_family,
+           base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace%
+    theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.line = element_line(colour = "black", size = rel(1)),
+          plot.title = element_text(size = base_size * 1.2, colour = "black", face = "bold", hjust = .5),
+          legend.key = element_blank(),
+          legend.title = element_blank(),
+          axis.text = element_text(colour = "black"),
+          axis.title.x = element_blank(),
+          axis.ticks = element_line(colour = "black"),
+          strip.background = element_rect(colour = "black", fill = NA))
+}
+
+my_colour <- c("#577DC7", "#C74B43", "#9BBB59", "#A5A5A5", "#55A3C8")
+
+my_colour_palette <- colorRampPalette(c("#969696", RColorBrewer::brewer.pal(8, "Set1")))
+
+
+# my_theme <- theme(
+#   legend.text = element_text(colour="black"),
+#   legend.title = element_blank(),
+#   plot.title = element_text(hjust = 0.5, face = "bold"),
+#   #axis.title.x = element_text(colour="black"),
+#   axis.title.x = element_blank(),
+#   axis.title.y = element_text(colour="black"),
+#   axis.text.x = element_text(colour ="black"),
+#   axis.text.y = element_text(colour="black"),
+#   axis.ticks.length = unit(0.4, "lines"),
+#   axis.ticks = element_line(colour = "black"),
+#   axis.line = element_line(linetype = "solid")
+# )
